0b7221fa8d007376dd010fc64895bd17d5ccd781
vadimpirov85@gmail.com <vadimpirov85@gmail.com>
2015-09-08 18:16:01 +0300

    Add min & max directive.

--- angular(personal-tasks)/js/directives.js
+++ angular(personal-tasks)/js/directives.js
@@ -1,5 +1,61 @@
 var managerDirectives = angular.module('managerDirectives', []);
 
+
+function isEmpty(value) {
+	return angular.isUndefined(value) || value === '' || value === null || value !== value;
+}
+
+managerDirectives.directive('ngMin', function () {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function (scope, elem, attr, ctrl) {
+			scope.$watch(attr.ngMin, function () {
+				ctrl.$setViewValue(ctrl.$viewValue);
+			});
+			var minValidator = function (value) {
+				var min = scope.$eval(attr.ngMin) || 0;
+				if (!isEmpty(value) && value < min) {
+					ctrl.$setValidity('ngMin', false);
+					return undefined;
+				} else {
+					ctrl.$setValidity('ngMin', true);
+					return value;
+				}
+			};
+
+			ctrl.$parsers.push(minValidator);
+			ctrl.$formatters.push(minValidator);
+		}
+	};
+});
+
+managerDirectives.directive('ngMax', function () {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function (scope, elem, attr, ctrl) {
+			scope.$watch(attr.ngMax, function () {
+				ctrl.$setViewValue(ctrl.$viewValue);
+			});
+			var maxValidator = function (value) {
+				var max = scope.$eval(attr.ngMax) || Infinity;
+				if (!isEmpty(value) && value > max) {
+					ctrl.$setValidity('ngMax', false);
+					return undefined;
+				} else {
+					ctrl.$setValidity('ngMax', true);
+					return value;
+				}
+			};
+
+			ctrl.$parsers.push(maxValidator);
+			ctrl.$formatters.push(maxValidator);
+		}
+	};
+});
+
+
 managerDirectives.directive('customModal', function($parse) {
 	return {
 		restrict: 'E',
@@ -57,6 +113,6 @@ managerDirectives.directive('customModal', function($parse) {
 			// 	element.append(clone);
 			// });
 
-		}
-	}
+}
+}
 });
