f556ec98b07a463c79b920f8e96d5018a117b02f
vadimpirov85@gmail.com <vadimpirov85@gmail.com>
2015-09-25 17:37:13 +0300

    LESS to CSS use GRUNT.

---
+++ grunt/less/Gruntfile.js
@@ -0,0 +1,28 @@
+﻿module.exports = function(grunt) {
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+		less: {
+			development: {
+				options: {
+					compress: false,
+					paths: ["content/css"]
+				},
+				files: {
+					"content/css/layout.css": "content/less/layout.less"
+				}
+			}
+		},
+		watch: {
+			styles: {
+				files: ['content/less/**/*.less'], // which files to watch
+				tasks: ['less'],
+				options: {
+					nospawn: true
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['less', 'watch']);
+};
