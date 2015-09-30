4e2d47843ec79f7c4f94f05d9be2e06dde842359
VadimZhdan <VadimZhdan@coherentsolutions.com>
2015-09-30 12:54:04 +0300

    Add filerev and usemin.

--- grunt/less/Gruntfile.js
+++ grunt/less/Gruntfile.js
@@ -2,6 +2,20 @@
 	require('jit-grunt')(grunt);
 
 	grunt.initConfig({
+		clean: {
+				build: {
+				src: ["dist/"]
+			}
+		},
+		copy: {
+			main: {
+				cwd: 'src/',
+				src: '**',
+				dest: 'dist/',
+				expand: true,
+				flatten: false
+			},
+		},
 		less: {
 			development: {
 				options: {
@@ -9,13 +23,42 @@
 					paths: ["content/css"]
 				},
 				files: {
-					"content/css/layout.css": "content/less/layout.less"
+					"dist/content/css/layout.css": "dist/content/less/layout.less"
 				}
+			}
+		},
+		autoprefixer: {
+			dist: {
+				files: {
+					'dist/content/css/layout.css': 'dist/content/css/layout.css'
+				}
+			}
+		},
+		filerev: {
+			options: {
+				//encoding: 'utf8',
+				algorithm: 'md5',
+				length: 8
+			},
+			source: {
+				files: [{
+					src: [
+						'dist/js/**/*.js',
+						'dist/content/css/**/*.css'
+					]
+				}]
+			}
+		},
+		usemin: {
+			html: ['dist/**/*.html'],
+			//html: ['dist/index.html', 'dist/test.html'],
+			options: {
+				assetsDirs: ['dist']
 			}
 		},
 		watch: {
 			styles: {
-				files: ['content/less/**/*.less'], // which files to watch
+				files: ['content/less/**/*.less'],
 				tasks: ['less'],
 				options: {
 					nospawn: true
@@ -23,6 +66,21 @@
 			}
 		}
 	});
+	
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-debug-task');
+	grunt.loadNpmTasks('grunt-filerev');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('default', ['less', 'watch']);
+
+	grunt.registerTask('default', ['clean', 'copy', 'less', 'autoprefixer', 'filerev', 'usemin']);
+	
+
+	// usemin replaces the references of scripts, stylesheets and other assets within HTML 
+	//   files dynamically with optimized versions of them.
+
+	//grunt.registerTask('default', ['less', 'autoprefixer', 'filerev', 'watch']);
 };
